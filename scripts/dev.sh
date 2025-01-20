947a792e067424ae77d33d9d00dadcbcf341c0b7
Chlorine <146422759+chlorine3545@users.noreply.github.com>
2025-01-20 16:12:06 +0800

    chore: add dev.sh script

---
+++ scripts/dev.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+# 创建软链接到主题目录
+cd exampleSite
+mkdir -p themes
+ln -sf ../.. themes/hermeneutics
+
+# 启动Hugo服务器
+hugo server \
+  --buildDrafts \
+  --disableFastRender \
+  --navigateToChanged \
+  --themesDir themes \
+  --theme hermeneutics \
+  --tlsCertFile /Users/chlorine/Dev/Utils/cert/localhost+2.pem \
+  --tlsKeyFile /Users/chlorine/Dev/Utils/cert/localhost+2-key.pem
